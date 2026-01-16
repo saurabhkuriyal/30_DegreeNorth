a0970ea697c2bf94b6e3d93353b71f984f334220
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-17 00:08:44 +0530

    modifying the banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import CardDemo from '@/components/custom/CardDemo';
 import Carousel from '@/components/custom/Carousel';
-
+import { NewBanner } from '@/components/custom/NewBanner';
 export default function Home() {
   return (
     <div>
       
       <Carousel />
+      <NewBanner/>
      <p className='text-2xl my-3'><em className='border-b-2 border-b-black rounded-r-2xl'>Here are our few treks...</em></p> 
       <CardDemo />
       
--- components/custom/NewBanner.tsx
+++ components/custom/NewBanner.tsx
@@ -3,12 +3,12 @@ import { ArrowRight } from "lucide-react"
 
 function NewBanner() {
   return (
-    <Banner variant="muted" className="dark text-foreground">
+    <Banner variant="muted" className="dark text-foreground my-2">
       <div className="w-full">
         <p className="flex justify-center text-sm">
           <a href="#" className="group">
             <span className="me-1 text-base leading-none">✨</span>
-            Introducing transactional and marketing emails
+            Take limited time discount on our next offbeat trek
             <ArrowRight
               className="-mt-0.5 ms-2 inline-flex opacity-60 transition-transform group-hover:translate-x-0.5"
               size={16}
