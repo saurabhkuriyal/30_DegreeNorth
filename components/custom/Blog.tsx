444cbd6c1b8fa355f0550b57f82d5b37183e73ee
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-21 00:19:24 +0530

    improving testimonials

--- components/custom/Blog.tsx
+++ components/custom/Blog.tsx
@@ -53,39 +53,39 @@ const posts = [
 
 export default function Example() {
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32 rounded-3xl">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">From the blog</h2>
-          <p className="mt-2 text-lg/8 text-gray-300">Learn how to grow your business with our expert advice.</p>
+          <h2 className="text-4xl font-semibold tracking-tight text-pretty text-black sm:text-5xl">Our Testimonials</h2>
+          <p className="mt-2 text-lg/8 text-gray-800">Be the part of our tribe.</p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-400">
+                <time dateTime={post.datetime} className="text-black">
                   {post.date}
                 </time>
                 <a
                   href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-gray-300 hover:bg-gray-800"
+                  className="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-black hover:bg-gray-800"
                 >
                   {post.category.title}
                 </a>
               </div>
               <div className="group relative grow">
-                <h3 className="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
+                <h3 className="mt-3 text-lg/6 font-semibold text-black group-hover:text-gray-800">
                   <a href={post.href}>
                     <span className="absolute inset-0" />
                     {post.title}
                   </a>
                 </h3>
-                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-400">{post.description}</p>
+                <p className="mt-5 line-clamp-3 text-sm/6 text-black">{post.description}</p>
               </div>
               <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
                 <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray-800" />
                 <div className="text-sm/6">
-                  <p className="font-semibold text-white">
+                  <p className="font-semibold text-black">
                     <a href={post.author.href}>
                       <span className="absolute inset-0" />
                       {post.author.name}
--- components/custom/News.tsx
+++ components/custom/News.tsx
@@ -29,24 +29,24 @@ const features: Feature[] = [
 
 export default function News() {
   return (
-    <div className="overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pt-4 lg:pr-8">
             <div className="lg:max-w-lg">
-              <h2 className="text-base/7 font-semibold text-indigo-400">Deploy faster</h2>
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
+              <h2 className="text-base/7 font-semibold text-indigo-800">Deploy faster</h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-black sm:text-5xl">
                 A better workflow
               </p>
-              <p className="mt-6 text-lg/8 text-gray-300">
+              <p className="mt-6 text-lg/8 text-black">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                 iste dolor cupiditate blanditiis ratione.
               </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-400 lg:max-w-none">
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-black lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-white">
-                      <feature.icon aria-hidden="true" className="absolute top-1 left-1 size-5 text-indigo-400" />
+                    <dt className="inline font-semibold text-black">
+                      <feature.icon aria-hidden="true" className="absolute top-1 left-1 size-5 text-indigo-800" />
                       {feature.name}
                     </dt>{' '}
                     <dd className="inline">{feature.description}</dd>
@@ -67,3 +67,6 @@ export default function News() {
     </div>
   )
 }
+
+
+//  bg-gray-900
