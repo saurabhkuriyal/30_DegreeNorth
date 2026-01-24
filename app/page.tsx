5d20eec12552b722bf37f847acb2d8b4f80fb779
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-24 23:39:26 +0530

    formatting structure of main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import News from '@/components/custom/News';
 export default function Home() {
   return (
     <div>
-      
+
       <Carousel />
-      <NewBanner/>
-     <p className='text-2xl my-3'><em className='border-b-2 border-b-black rounded-r-xl'>Here are our few treks...</em></p> 
+      <NewBanner />
+      <p className='text-2xl my-3'><em className='border-b-2 border-b-black rounded-r-xl'>Here are our few treks...</em></p>
       <CardDemo />
-      <p className='text-2xl my-3'><em className='border-b-2 border-b-black rounded-r-xl'>Look at our stories</em></p> 
+      <p className='text-2xl my-3'><em className='border-b-2 border-b-black rounded-r-xl'>Look at our stories</em></p>
       <News />
-      <Blog/>
-      
+      <Blog />
+
     </div>
   );
 }
