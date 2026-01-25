4fb77651b048bd707d5a7923c8505f247fc892ea
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-25 18:19:05 +0530

    making final changes in News section

--- components/custom/News.tsx
+++ components/custom/News.tsx
@@ -1,32 +1,8 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
+
 import { CalendarDays, MapPin } from 'lucide-react';
 
-// 1. Define the shape of a Feature object
-interface Feature {
-  name: string
-  description: string
-  icon: React.ElementType
-}
 
-// 2. Apply the type to the array
-const features: Feature[] = [
-  {
-    name: 'Push to deploy.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
-  },
-]
+
 
 export default function News() {
   return (
